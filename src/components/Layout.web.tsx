f5afeb9851aad99f3fc4e692787327d03c1d8b6a
Paulo Solipa <psolipa@product-league.com>
2023-12-07 11:38:10 +0000

    First Commit

---
+++ src/components/Layout.web.tsx
@@ -0,0 +1,179 @@
+export default function Layout({ children }) {
+    return (
+        <>
+            <header data-advancedhtml="" role="banner" className="header" id="b1-Header2">
+                <div data-container="" className="ThemeGrid_Container">
+                    <div data-container="" className="header-topbar" id="b1-HeaderTopBar">
+                        <div data-block="AA_Common.LoadHTML" className="OSBlockWidget" id="b1-$b1">
+                            <div data-container="" id="b1-b1-HTMLWrapper">
+                                <div data-container="" className="cf-header padding-y-xs">
+                                    <div data-block="Adaptive.ColumnsMediumRight" className="OSBlockWidget" id="b1-$b1">
+                                        <div data-container=""
+                                            className="columns columns-medium-right gutter-base tablet-break-none phone-break-none font-size-xs align-items-center">
+                                            <div className="columns-item" id="b1-b1-Column1">
+                                                <div data-list="" className="list list-group OSFillParent">
+                                                    <div data-container="" className="cf-header__usp margin-right-base display-inline-flex three-usps"
+                                                        style={{padding: 0}}>
+                                                        <div data-block="CustomPatterns.IconPlus" className="OSBlockWidget" id="b1-l1-19_0-$b2">
+                                                            <div data-container="" className="display-inline-flex"><i data-advancedhtml=""
+                                                                className="icon-plus icon-plus-check text-primary"></i></div>
+                                                        </div>
+                                                        <div data-block="StaticContent.LinkWrapper_Web" className="OSBlockWidget" id="b1-l1-19_0-$b3"><a
+                                                            data-link="" className="cf-link-wrapper-web " href="https://ecop.plus.nl/bestellen/bezorgen"
+                                                            target="" id="b1-l1-19_0-b3-LinkElement">
+                                                            <div className="cf-link-wrapper-web__content" id="b1-l1-19_0-b3-LinkWrapper"><span
+                                                                data-expression="" className="margin-left-xs font-size-xs text-neutral-7">Google</span></div>
+                                                        </a></div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div className="columns-item" id="b1-b1-Column2">
+                                                <div data-container="" className="text-align-right">
+                                                    <div data-list="" data-virtualization-disabled=""
+                                                        className="list list-group display-flex justify-content-flex-end OSFillParent"
+                                                        disable-virtualization="True">
+                                                        <div data-container="" className="cf-header__quick-link margin-left-base OSInline"
+                                                            style={{padding: 0}}>
+                                                            <div data-block="StaticContent.LinkWrapper_Web" className="OSBlockWidget" id="b1-l2-20_0-$b4"><a
+                                                                data-link="" className="cf-link-wrapper-web " href="https://ecop.plus.nl/folder" target=""
+                                                                 id="b1-l2-20_0-b4-LinkElement">
+                                                                <div className="cf-link-wrapper-web__content" id="b1-l2-20_0-b4-LinkWrapper"><span
+                                                                    data-expression="" className="font-size-xs">Folder</span></div>
+                                                            </a></div>
+                                                        </div>
+                                                        <div data-container="" className="cf-header__quick-link margin-left-base OSInline"
+                                                            style={{padding: 0}}>
+                                                            <div data-block="StaticContent.LinkWrapper_Web" className="OSBlockWidget" id="b1-l2-20_1-$b4"><a
+                                                                data-link="" className="cf-link-wrapper-web " href="/supermarkten" target=""
+                                                                id="b1-l2-20_1-b4-LinkElement">
+                                                                <div className="cf-link-wrapper-web__content" id="b1-l2-20_1-b4-LinkWrapper"><span
+                                                                    data-expression="" className="font-size-xs">Winkels</span></div>
+                                                            </a></div>
+                                                        </div>
+                                                        <div data-container="" className="cf-header__quick-link margin-left-base OSInline"
+                                                            style={{padding: 0}}>
+                                                            <div data-block="StaticContent.LinkWrapper_Web" className="OSBlockWidget" id="b1-l2-20_2-$b4"><a
+                                                                data-link="" className="cf-link-wrapper-web " href="https://ecop.plus.nl/consumentenservice"
+                                                                target=""  id="b1-l2-20_2-b4-LinkElement">
+                                                                <div className="cf-link-wrapper-web__content" id="b1-l2-20_2-b4-LinkWrapper"><span
+                                                                    data-expression="" className="font-size-xs">Klantenservice</span></div>
+                                                            </a></div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div data-container="" className="header-top">
+                    <div data-container="" className="header-content ThemeGrid_Container display-flex ">
+                        <div data-container="" className="menu-icon menu-icon-line" aria-label="Toggle the Menu" role="button"
+                            aria-haspopup="true" style={{cursor: "pointer"}}><span className="menu-icon-label">MENU</span></div><a data-link=""
+                                href="/">
+                            <div data-container=""
+                                className="layout-application-title application-name display-flex align-items-center margin-right-l full-height gtm-application-logo"
+                                id="b1-ApplicationTitleWrapper"></div>
+                        </a>
+                        <div data-container=""
+                            className="header-navigation columns columns-medium-right gutter-none tablet-break-none phone-break-all  align-items-center"
+                            id="b1-Header">
+                            <div data-container="" className="columns-item header-navigation-right center-right display-flex full-height">
+                                <button data-button="" className="btn login-button gtm-open-login" type="button">
+                                    <div data-block="CustomPatterns.IconPlus" className="OSBlockWidget" id="b1-$b7">
+                                        <div data-container="" className="display-inline-flex"><i data-advancedhtml=""
+                                            className="icon-plus icon-plus-account-logout icon-lg font-size-h4"></i></div>
+                                    </div><span data-expression="" className="margin-left-base no-wrap">Login</span>
+                                </button>
+                                <div data-container="" className="margin-left-base OSInline">
+                                    <div data-block="Cart.CartBadge" className="OSBlockWidget" id="b1-$b8"><a aria-current="page"
+                                        className="gtm-header-cart-link cart-badge-link active" data-link="" href="/winkelwagen">
+                                        <div data-container="" className="icon-badge cart-badge badge-hide-number">
+                                            <div data-container="">
+                                                <div data-block="CustomPatterns.IconPlus" className="OSBlockWidget" id="b1-b8-$b1">
+                                                    <div data-container="" className="display-inline-flex"><i data-advancedhtml=""
+                                                        className="icon-plus icon-plus-cart"></i></div>
+                                                </div><span data-expression="" className="cart-badge-price margin-left-s">0.00</span>
+                                            </div>
+                                            <div data-container="" className="badge background-red"><span data-expression="">0</span></div>
+                                        </div>
+                                    </a></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div data-container="" className="ThemeGrid_Container header-menubar-wrapper">
+                    <div data-container="" className="ph header-menubar padding-top-base" id="b1-HeaderMenuBar">
+                        <div data-block="Adaptive.ColumnsMediumRight" className="OSBlockWidget" id="b1-$b9">
+                            <div data-container=""
+                                className="columns columns-medium-right gutter-base tablet-break-none phone-break-none align-items-center menu-wrapper">
+                                <div className="columns-item" id="b1-b9-Column1">
+                                    <div data-block="AA_Common.Menu" className="OSBlockWidget" id="b1-$b10">
+                                        <nav data-advancedhtml="" className="app-menu-content display-flex gtm-navigation-menu" role="navigation">
+                                            <div data-container="" className="app-menu-header">
+                                                <div data-container="" className="app-menu-header--title"><span data-expression="">Menu</span></div>
+                                                <div data-container="" className="app-menu-header--close OSInline"><a data-link="" href="#">
+                                                    <div data-block="CustomPatterns.IconPlus" className="OSBlockWidget" id="b1-b10-$b1">
+                                                        <div data-container="" className="display-inline-flex"><i data-advancedhtml=""
+                                                            className="icon-plus icon-plus-close"></i></div>
+                                                    </div>
+                                                </a></div>
+                                            </div>
+                                            <div data-container="" className="app-menu-links" role="menubar" id="b1-b10-PageLinks">
+                                                <div data-container="" className="global-links-wrapper ">
+                                                    <div data-container="" className="menu-links" id="b1-b10-MenuLinksContainer">
+                                                        <div data-container="" className="link-wrapper">
+                                                            <div data-block="Navigation.Submenu" className="OSBlockWidget" id="b1-b10-ProductsSubmenu">
+                                                                <div data-container="" className="osui-submenu products-submenu osui-submenu--is-dropdown"
+                                                                     id="b1-b10-b2-Submenu">
+                                                                    <div data-container="" className="osui-submenu__header needsclick" id="b1-b10-b2-SubMenuHeader"
+                                                                        aria-haspopup="true"  aria-controls="b1-b10-b2-Items" role="button"
+                                                                        aria-expanded="false">
+                                                                        <div className="osui-submenu__header__item" id="b1-b10-b2-Menu">
+                                                                        Producten</div>
+                                                                </div>
+
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                    </div>
+                                </nav>
+                            </div>
+                        </div>
+                        <div className="columns-item" id="b1-b9-Column2">
+                            <div data-block="StoreFlow.StoreInfoDropdown" className="OSBlockWidget" id="b1-$b11">
+                                <div data-container="" className="header-store-info-wrapper gtm-store-info-popover">
+                                    <div data-block="PL_PopperJS.Popper" className="OSBlockWidget" id="b1-b11-Popper">
+                                        <div data-container="" className="popper-relative">
+                                            <div data-container="" className="popper-wrapper OSInline" id="b1-b11-b1-PopperItemWrapper">
+                                                <div className="popper-item OSInline" data-popper-item="closed" id="b1-b11-b1-PopperItem">
+                                                    <div data-block="CustomPatterns.IconPlus" className="OSBlockWidget" id="b1-b11-$b2">
+                                                        <div data-container="" className="display-inline-flex"><i data-advancedhtml=""
+                                                            className="icon-plus icon-plus-location"></i></div>
+                                                    </div><span className="margin-left-xs">Choose your store</span>
+                                                    <div data-block="CustomPatterns.IconPlus" className="OSBlockWidget" id="b1-b11-$b3">
+                                                        <div data-container="" className="display-inline-flex"><i data-advancedhtml=""
+                                                            className="icon-plus icon-plus-arrow-down font-size-xs margin-left-s"></i></div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div >
+</header >
+        <div className="content">{children}</div>
+        </>
+    )
+}
