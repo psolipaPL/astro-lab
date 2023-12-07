d2808cfeae701ea6aa6225a7eb81036d5795140c
Paulo Solipa <psolipa@product-league.com>
2023-12-07 11:43:39 +0000

    Astro vercel added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
+import vercel from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
 export default defineConfig({
 	// Enable React to support React JSX components.
 	integrations: [react()],
+	output: 'server',
+	adapter: vercel()
 });
